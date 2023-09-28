b42228bb503631ef0e85b140af3f7708eab95413
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-28 18:51:49 +0200

    .

--- src/routes/chess/+page.svelte
+++ src/routes/chess/+page.svelte
@@ -1,44 +1,43 @@
 <script>
     import { Chess } from 'chess.js'
-    const initialboard = 'rnbqkbnr/ppppppp1/8/7p/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-    var st = null, cb, next, hist=[], mate
+    var st = null, cb, next, hist=[], mate, iam='w'
     const chess = new Chess()
     cb = chess.board()
-    next=chess.turn()
+    next = chess.turn()
     var cf = new Map([
         ['p', '♟'],['r', '♜'],['n', '♞'],['b', '♝'],['q', '♛'],['k', '♚'], [null, ' ']
     ])
-    const checkmate = () => chess.isCheckmate() ? 'Matt' : chess.isDraw() ? 'Döntetlen'  : ''
-    function drop(loc) {
-        try {
-            chess.move(st.square+"x"+loc)
-            fetch('https://stockfish.online/api/stockfish.php?fen='+chess.fen()+'&depth=11&mode=bestmove')
-                .then(v => v.json())
-                .then( v => {
-                    chess.move(v.data)
-                    cb = chess.board()
-                    hist = chess.history()
-                    next=chess.turn()
-                    mate = checkmate()
-                })
-        } catch(e) {
-            if (st.type != 'p' || loc[0] == st.square[0]) try {
-                chess.move(st.type!='p'?st.type.toUpperCase()+loc:loc)
-                fetch('https://stockfish.online/api/stockfish.php?fen='+chess.fen()+'&depth=11&mode=bestmove')
-                    .then(v => v.json())
-                    .then( v => {
-                        chess.move(v.data)
-                        cb = chess.board()
-                        hist = chess.history()
-                        next=chess.turn()
-                        mate = checkmate()
-                    })
-            } catch(e) {}
-        }
+    const undo = () => {
+        chess.undo()
+        chess.undo()
+        refresh()
+    }
+    const refresh = () => {
         cb = chess.board()
         hist = chess.history()
         next = chess.turn()
         mate = checkmate()
+    }
+    const fetcfish = () => fetch('https://stockfish.online/api/stockfish.php?fen='
+        +chess.fen()
+        +'&depth=11&mode=bestmove')
+        .then(v => v.json())
+        .then( v => {
+            chess.move(v.data.split(" ")[1])
+            refresh()
+        })
+    const checkmate = () => chess.isCheckmate() ? 'Matt' : chess.isDraw() ? 'Döntetlen'  : ''
+    function drop(loc) {
+        try {
+            chess.move(st.square+"x"+loc)
+            fetcfish()
+        } catch(e) {
+            if (st.type.toLowerCase() != 'p' || loc[0].toLowerCase() == st.square[0].toLowerCase()) try {
+                chess.move('g1f3')
+                fetcfish()
+            } catch(e) {console.log(e)}
+        }
+        refresh()
     }
 </script>
 {#if mate}
@@ -46,15 +45,18 @@
 {:else}
 <h1>Sakk</h1> 
 {/if}
-<table class={mate?'red':next=='w'?'norm':'yt'}>
+<button on:click={() => undo()}>Visszalép</button>
+<table class={mate?'red':next == iam ? 'norm' : 'yt'}>
     {#each cb as cr, i}
         <tr>
             {#each cr as c, j}
-                <td class={`x`+(i+j)%2 + ' ' + next}
-                    on:drop={() => drop(String.fromCharCode(97+j)+(8-i))}
+                <td class={`x`+(i+j)%2 + ' ' + (next == iam ? 'iam' : '')}
+                    on:drop={() => {
+                        drop(String.fromCharCode(97+j)+(8-i))
+                    }}
                     on:dragover={e => (e.preventDefault(), true)}>
                     <!-- svelte-ignore a11y-no-static-element-interactions -->
-                    <span draggable={next=='w' && !mate ? true : false}
+                    <span draggable={next == iam && !mate ? true : false}
                         on:dragstart={() => st = c} 
                         class={c && c.color}>{cf.get(c && c.type)}</span>
                 </td>
@@ -71,6 +73,9 @@
     table {
         margin: auto;
         border-spacing: 8px;
+        border-radius: 12px;
+        box-shadow: 1px 1px 3px inset black;
+        padding: 15px;
     }
     :global(table.red) {
         background-color: rgb(210, 98, 98);
@@ -82,11 +87,11 @@
         background-color: rgb(158, 195, 227);
     }
     td {
-        color: rgb(6, 67, 81);
+        color: rgb(6, 40, 47);
         font-size: 40px;
         width: 50px;
         height: 50px;
-        background-color: rgb(202, 195, 184);
+        background-color: rgb(225, 223, 219);
         box-shadow: 1px 1px 3px black;
         border-radius: 8px;
     }
@@ -94,26 +99,22 @@
         cursor: pointer;
     }
     td.x1 {
-        background-color: rgb(148, 174, 174);
+        background-color: rgb(97, 120, 120);
     }
     td span {
+        display: inline-block;
         margin: 0px;
         padding: 0px;
-        margin-top: -10px;
+        margin-top: -20px;
         cursor: grab;
         background: transparent;
-        text-shadow: 1px 1px 3px white;
+        text-shadow: 0px 0px 4px white;
     }
     td span.w {
-        margin: 0px;
-        padding: 0px;
-        margin-top: -10px;
-        cursor: grab;
-        background: transparent;
         color:rgb(224, 220, 121);
-        text-shadow: 1px 1px 3px black;
+        text-shadow: 0px 0px 4px black;
     }
-    td.b span {
+    td.aim span {
         cursor: default;
     }
     span.hist {
@@ -127,8 +128,26 @@
         display: inline-block;
         max-width: 80%;
         color: green;
+        min-height: 10px;
     }
     h1.matt {
         color: brown;
+    }
+    button {
+        all: unset;
+        margin: 5px;
+        border-radius: 7px;
+        box-shadow: 1px 1px 3px black;
+        background-color: aqua;
+        padding: 6px;
+        cursor: pointer;
+    }
+    button:hover {
+        box-shadow: 1px 1px 3px black;
+        background-color: rgb(114, 175, 175);
+    }
+    button:active {
+        box-shadow: 1px 1px 3px black;
+        background-color: rgb(255, 89, 0);
     }
 </style>
